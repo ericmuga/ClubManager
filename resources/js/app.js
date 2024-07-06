101aafd8e142aa6882992b7209242eff8afa4086
Eric Muga <eric.muga@gmail.com>
2024-07-06 16:40:57 +0300

    added primevue

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,27 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+// primevue components
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primevue/resources/themes/lara-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import MultiSelect from 'primevue/multiselect';
+import Toolbar from 'primevue/toolbar';
+import Button from 'primevue/button';
+import Pagination from '@/Components/Pagination.vue'
+import InputText from 'primevue/inputtext'
+import InputNumber from 'primevue/inputnumber'
+import Tooltip from 'primevue/tooltip';
+import Checkbox from 'primevue/checkbox';
+import Calendar from 'primevue/calendar';
+import Dropdown from 'primevue/dropdown';
+import Password from 'primevue/password';
+import Badge from 'primevue/badge';
+
+///end of primevue components
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -15,6 +36,24 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+             })
+            .component('ToolBar',Toolbar)
+            .component('Checkbox',Checkbox)
+            .component('Button',Button)
+            .component('InputText',InputText)
+            .component('InputNumber',InputNumber)
+            .directive('Tooltip',Tooltip)
+            .component('MultiSelect',MultiSelect)
+            .component('Dropdown',Dropdown)
+            .component('Pagination',Pagination)
+            .component('SearchBox',SearchBox)
+            .component('Calendar',Calendar)
+            .component('Password',Password)
+            .component('Badge',Badge)
             .mount(el);
     },
     progress: {
