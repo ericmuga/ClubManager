dcf8558aad89992b2571a5dc9625b53cb8edc8b0
Eric Muga <eric.muga@gmail.com>
2024-09-03 07:59:59 +0300

    added default admin FK

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,5 +25,13 @@ class DatabaseSeeder extends Seeder
         $user->phone='0720816931';
         $user->password=bcrypt('Tester123');
         $user->save();
+
+        $user=User::orderBy('id','desc')->first();
+        $user->email='fkinuthia@farmerschoice.co.ke';
+        $user->name='Flora Kinuthia';
+        $user->user_type='member';
+        $user->phone='0706561813';
+        $user->password=bcrypt('rotary2024$');
+        $user->save();
     }
 }
